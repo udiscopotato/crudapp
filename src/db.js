1e6d758ee4350f629b6d21eb4f694249cee459fa
Subham Jena <103844138+udiscopotato@users.noreply.github.com>
2023-06-01 17:27:40 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
+  host: "customer.cwowl7yn35i4.us-east-1.rds.amazonaws.com",
+  user: "admin",
   password: "bapun123",
   port: 3306,
   database: "customersdb",
