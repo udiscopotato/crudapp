1354685539cf6b0cb5dca9be581acad892b7fef9
Subham Jena <103844138+udiscopotato@users.noreply.github.com>
2023-06-04 20:09:57 +0530

    modified src as per docker standard

--- src/db.js
+++ src/db.js
@@ -1,9 +1,15 @@
 import { createPool } from "mysql2/promise";
 
+var mysqlhost = process.env.MYSQL_HOST || 'customer.cwowl7yn35i4.us-east-1.rds.amazonaws.com';
+var mysqluser = process.env.MYSQL_USER || 'admin';
+var mysqlpassword = process.env.MYSQL_PASSWORD || "bapun123";
+
 export const pool = createPool({
-  host: "customer.cwowl7yn35i4.us-east-1.rds.amazonaws.com",
-  user: "admin",
-  password: "bapun123",
+  host: mysqlhost,
+  user: mysqluser,
+  password: mysqlpassword,
   port: 3306,
   database: "customersdb",
 });
+
+
